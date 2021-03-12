baeac63d93063c8ff9dd84a9c432ba7d43c299f1
Mazlum Altay <mazlumaltayy@outlook.com>
2021-03-12 14:35:55 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager:BaseCustomerManager
+
+    {
+        private ICustomerCheckService _customerCheckService;
+
+        public StarbucksCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+
+        public override void save(Customer customer)
+        {
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.save(customer);
+            }
+            
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.save(new Customer {DateOfBrith = new DateTime(1985,1,6), FirstName = "Engin", LastName = "Altay", NationalityId = "288614" });
+
+        }
+    }
+}
